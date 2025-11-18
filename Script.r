1026294162bebdc4fd6b4fc42a8a25d9c1da7d48
Esilvas <edwinsils@outlook.com>
2025-11-18 15:31:51 -0500

    Modificacion y calculo de said enero 2024

--- Script.r
+++ Script.r
@@ -3,34 +3,26 @@
 library(dplyr)
 library(readr)
 
-#calcular indicador SAIDI enero
+# Leer el archivos CS2 y TC1 para enero con locale para manejar encoding
+cs2_enero_2024 <- read_csv("DATA/CENS/2024/CS2_ENE_2024_OR_604.CSV", 
+                           show_col_types = FALSE,
+                           locale = locale(encoding = "Latin1"))
+
+tc1_enero_2024 <- read_csv("DATA/CENS/2024/TC1_OR_604_ENE_2024.csv", 
+                           show_col_types = FALSE,
+                           locale = locale(encoding = "Latin1"))
 
 
+#crear dataframe solo con dos columnas NIU y DIUM
+cs2_enero_2024_subset <- cs2_enero_2024 %>%
+  select(NIU, DIUM)
+#eliminar duplicados de cs2_enero_2024_subset
+cs2_enero_2024_subset <- cs2_enero_2024_subset %>%
+  distinct()    
 
+#sumar la columna DIUM
+total_dium <- sum(cs2_enero_2024_subset$DIUM, na.rm = TRUE) 
 
-# Definir la ruta de los archivos
-ruta_cs2 <- "\\\\CENS-AD02.cens.corp.epm.com.co\\Administrativa\\UO01\\CISO01\\CISO01\\atenea\\nueva descarga 18-11-2025\\CS2\\2025"
+saidi <- total_dium/nrow(tc1_enero_2024)
 
-# Listar todos los archivos CSV en la carpeta
-archivos_csv <- list.files(path = ruta_cs2, 
-                           pattern = "\\.csv$", 
-                           full.names = TRUE)
-
-
-# Leer y consolidar todos los archivos CSV en uno solo
-datos_cs2 <- archivos_csv %>%
-  lapply(read_csv, show_col_types = FALSE) %>%
-  bind_rows()
-
-# Mostrar información del dataset consolidado
-cat("Total de archivos leídos:", length(archivos_csv), "\n")
-cat("Total de registros consolidados:", nrow(datos_cs2), "\n")
-cat("Total de columnas:", ncol(datos_cs2), "\n")
-
-# Mostrar primeras filas
-head(datos_cs2)
-
-
-
-
-#rmarkdown::render("Informe_ATENEA.Rmd")
+saidi
