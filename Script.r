3d9de28caf8aadeae06bc785615479cdfca9d9f5
Esilvas <edwinsils@outlook.com>
2025-11-20 09:45:47 -0500

    Ejecucion del proecdiemento para genera los informes nuevamente

--- Script.r
+++ Script.r
@@ -302,7 +302,6 @@ escenario_1 <- analizar_escenarios(c(1))
 
 print(escenario_1)
 
-rmarkdown::render("Informe_ATENEA.Rmd", params = list(grupos_seleccionados = c(6)))
+rmarkdown::render("Informe_ATENEA.Rmd", params = list(grupos_seleccionados = c(4,5,6)))
 
 # nolint end
-
